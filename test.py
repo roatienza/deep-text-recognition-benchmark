fe33811c06244947714359e598f3a8eef9a66443
Rowel Atienza <rowel.atienza@gmail.com>
2020-12-26 12:44:39 +0800

    word

--- test.py
+++ test.py
@@ -120,15 +120,12 @@ def validation(model, criterion, evaluation_loader, converter, opt):
             preds_str = converter.decode(preds_index.data, preds_size.data)
         
         elif opt.Transformer:
-            #preds = model(text_for_loss)
             preds = model(image, text=target, seqlen=converter.batch_max_length)
             _, preds_index = preds.topk(1, dim=-1, largest=True, sorted=True)
             preds_index = preds_index.view(-1, converter.batch_max_length)
             forward_time = time.time() - start_time
-            #target, length_for_loss = converter.encode(labels, batch_max_length=opt.batch_max_length, is_train=False)
             cost = criterion(preds.contiguous().view(-1, preds.shape[-1]), target.contiguous().view(-1))
 
-            # select max probabilty (greedy decoding) then decode index to character
             length_for_pred = torch.IntTensor([converter.batch_max_length - 1] * batch_size).to(device)
             preds_str = converter.decode(preds_index[:, 1:], length_for_pred)
         else:
