329f6ea461f3eae6e213edfdb1731f53e1bde1cb
Rowel Atienza <rowel.atienza@gmail.com>
2021-01-10 16:44:24 +0800

    prob

--- train.py
+++ train.py
@@ -301,7 +301,7 @@ if __name__ == '__main__':
     parser.add_argument('--data_augment', action='store_true', help='If train data augmentation will be used')
     parser.add_argument('--rotation', action='store_true', help='Enable rotation dats aug')
     parser.add_argument('--rotation_prob', default=0.5, help='Rotation prob')
-    parser.add_argument('--rotation_angle', default=34., help='Rotation angle std in deg')
+    parser.add_argument('--rotation_angle', default=20., help='Rotation angle std in deg')
     parser.add_argument('--perspective', action='store_true', help='Perspective')
     parser.add_argument('--perspective_prob', default=0.3, help='Perspective probability')
     parser.add_argument('--warp', action='store_true', help='Image warping')
