91bd6aea56b927805c53d3786681dcade3ca1241
Rowel Atienza <rowel.atienza@gmail.com>
2021-02-09 10:40:20 +0800

    10k

--- train.py
+++ train.py
@@ -255,7 +255,7 @@ def train(opt):
                 log.write(predicted_result_log + '\n')
 
         # save model per 1e+5 iter.
-        if (iteration + 1) % 1e+5 == 0:
+        if (iteration + 1) % 1e+4 == 0:
             torch.save(
                 model.state_dict(), f'./saved_models/{opt.exp_name}/iter_{iteration+1}.pth')
 
